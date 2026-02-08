e167031a219942b885703fadebf71b0ad77348d7
Shreyansh Pande <179868648+shreyanshVIT23@users.noreply.github.com>
2026-02-09 00:40:42 +0530

    added config module for general shareable content

---
+++ src/config/config_env.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
