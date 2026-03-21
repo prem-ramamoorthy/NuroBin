ee20018b4b592e405aceed97fbb7d65f8a9a074d
Shreyansh Pande <179868648+shreyanshVIT23@users.noreply.github.com>
2026-03-21 22:25:10 +0530

    added routes to router list

--- Backend/src/api/__init__.py
+++ Backend/src/api/__init__.py
@@ -1,7 +1,19 @@
 from src.api.routes.patient_route import patient_router
 from src.api.routes.doctor_route import doctor_router
 from src.api.routes.caretaker_routes import caretaker_router
+from src.api.routes.meetings_routes import meeting_router
+from src.api.routes.caretaker_patient_link import caretaker_patient_link_router
+from src.api.routes.doctor_patient_link_routes import doctor_patient_link_router
 from src.gmaps.gmapsRouter import app as gmaps_router
 from src.video.route import router as face_router
 
-routers = [patient_router, doctor_router, caretaker_router, gmaps_router, face_router]
+routers = [
+    patient_router,
+    doctor_router,
+    caretaker_router,
+    gmaps_router,
+    face_router,
+    meeting_router,
+    caretaker_patient_link_router,
+    doctor_patient_link_router,
+]
